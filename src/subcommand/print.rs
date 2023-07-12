bcb49ee891cdb456632a7cfd5eff54e841b8fc6a
Bryan McClain <bamcclain10@gmail.com>
2023-07-12 01:05:05 -0400

    Ability to print to HTML file

--- src/subcommand/print.rs
+++ src/subcommand/print.rs
@@ -1,5 +1,7 @@
 use clap::Args;
 use diesel::SqliteConnection;
+use std::io::Write;
+use tempfile::Builder;
 use termimad::MadSkin;
 
 use crate::models::Recipe;
@@ -8,17 +10,43 @@ use crate::models::Recipe;
 pub struct PrintArgs {
   /// Integer ID of the recipe to delete
   id: i32,
+
+  #[clap(short, long)]
+  web: bool,
 }
 
 impl PrintArgs {
   pub fn execute(self, conn: &mut SqliteConnection) -> super::Result<()> {
-    match Recipe::find_optional(&self.id, conn)? {
-      None => println!("No such recipe: {}", self.id),
-      Some(recipe) => {
-        let skin = MadSkin::default();
-        skin.print_text(&recipe.markdown_string());
+    let recipe = match Recipe::find_optional(&self.id, conn)? {
+      None => {
+        println!("No such recipe: {}", self.id);
+        return Ok(());
       },
+      Some(recipe) => recipe,
+    };
+
+    if !self.web {
+      let skin = MadSkin::default();
+      skin.print_text(&recipe.markdown_string());
+      return Ok(());
     }
+
+    let html = markdown::to_html(&recipe.markdown_string());
+    let mut file = Builder::new().suffix(".html").tempfile()?;
+    write!(
+      file,
+      r#"
+<html>
+  <head>
+    <title>{recipe}</title>
+  </head>
+  <body>{body}</body>
+</html>"#,
+      recipe = recipe.name,
+      body = html
+    )?;
+
+    open::that(file.path())?;
 
     Ok(())
   }
